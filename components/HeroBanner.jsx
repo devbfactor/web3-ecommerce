b6f2de4eaafa95e448ec4b481c1573005d6ce92e
paolanocom <paolanocom@gmail.com>
2023-05-03 14:07:12 +0800

    Updated hero banner, product currency

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -7,17 +7,17 @@ const HeroBanner = ({ heroBanner }) => {
   return (
     <div className='hero-banner-container'>
       <div>
-        <p className='beats-solo'>{heroBanner.smallText}</p>
+        <p className='small-text'>{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt='headphones' className='hero-banner-image' />
+        <img src={urlFor(heroBanner.image)} alt='Featured Video Game' className='hero-banner-image' />
 
         <div>
           <Link href={`/product/${heroBanner.product}`}>
             <button type='button'>{heroBanner.buttonText}</button>
           </Link>
           <div className='desc'>
-            <h5>Description</h5>
+            {/* <h5>Description</h5> */}
             <p>{heroBanner.desc}</p>
           </div>
         </div>
