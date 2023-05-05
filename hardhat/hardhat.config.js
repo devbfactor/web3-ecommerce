3ea8e650b99099d1ea2e399a935277685d0e163c
devbfactor <dev.bfactor@gmail.com>
2023-05-05 08:35:40 +0800

    commit

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const ALCHEMY_HTTP_URL = process.env.ALCHEMY_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const MUMBAI_API = process.env.MUMBAI_API;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: ALCHEMY_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: MUMBAI_API,
+  }
 };
